453fc73ad0b2f619e627db3c282272793e22ed32
qnqnban <46470610+qnqnban@users.noreply.github.com>
2019-01-08 11:39:35 +0800

    Add files via upload

---
+++ cur/setting.js
@@ -0,0 +1,82 @@
+
+var banner_fastival=[	//[month,day,startDayToShow,endDayToShow,"http://lh3.googleusercontent.com/-l-LbYGsaFs0/VIUg5p_je8I/AAAAAAAAAL0/qSlHPjbZr7A/s470/xmas_2014.png", "http://q9tech.com"],
+						[12,25,-14,3,"https://lh3.googleusercontent.com/z-B9XTX0e54DZSRmxV2PqSpQtxCLwhutN2GiFeZjP7zb7Qvfm0uR80i9_ZUd6SkUDDI2mt32jIYQWO65xSgSKyXFAYXYplK1bwzeN_01P57mSEEsOceJYxHJ5B6jQzHeoGwOiARqng=w470-h210-no", "http://q9tech.com"],
+						[1,1,0,1,"https://lh3.googleusercontent.com/Bkm1N2cFMGHi7JhNgynMfo3nsHI5tL9BsHKA7q4Cw2B5pxk4L2wcBe9Y3kVgDThxEEaQLuImGqX7ga3swg5Yi-bo4Hty_20bCmdeVgzYT73gYQnk_VDLtWN0s1mcWoynIFwv0o5LUudzZ8j4blstVTBqtqUjJpXv6U0Q0WA2Lxi5oypFcy8A7AOw3A8HNiVaf5FGpc7KWhrAiuGbD-NwjbI_0_anjGGS5ZQ747E_KckcgojKUAqYYR8L5dqiC2Cr50nTl-qiNC97X8Rn64CEvI0hB9T80r4i4_RN1bMpqV2p9w5avWRmoqYT4tDyjt9BHbhXL2FobrK-b883lUvyqkSJJ22WHtFeEcZ8oBk36Hhn3Wl0LtwSrrr4kLZ8c-yOeNKZ2iLX27lxy3QfGO1Du1XARy7UdfRAZ8gKJ9GgDXriVdazIRXzrxS4gLqB0U4ke7tnTbpBu-0WhM0fErz5BGHP70mldbxtAQMkio3ZeC6HHuYYWQUHDqCMPNbZ8p2HGLnfXAQ2tfJqIlEt4bJD6ZTsf5tZiKXtT9fNFs1otc--kr9iEFuhyCrb9pITGTQrehaRtrm48GaAn8i9cc7nZjF8z-xGJ9_3wQL51mvnsSr3FggfytpdIQYDJwcxuJT8kXOQcs6KHL-Fp6C3i4tatnXHWreoLQr6hnC9fgsJ2A=w470-h210-no", "http://q9tech.com"],
+						[2,5,-6,10,"https://lh3.googleusercontent.com/j3rD25pmxwUnNp_cJs6S_YzQXadn8_CbCYQru1kNhsyeN1AhulsGES7wdQtVTrA8d3o0IPj5KOBBSxlDFJRdaikPPHbaUaEAkDii9xqpQdzB8rfvhRkHyN-1yOIqWvT24w1pOw=w470-h210-no", "http://q9tech.com"]	,
+						[3,30,0,7,"http://lh4.googleusercontent.com/-aZSevOBFcOM/U098Dab0NyI/AAAAAAAAAJc/GaVID2Rm8_8/w470-h210-no/Q9_2014_easter.png", "http://q9tech.com"]		
+						
+						];
+/*
+var banners = [	
+				["Q9WirelessKeyboard.gif",	"http://www.q9tech.com/web10/productIntro/retailSoftware/qd_ebook_1_mobile/swdetail.php?pid=110"],				
+				["Q9_iOS8.png",	"http://www.q9tech.com/web10/productIntro/retailSoftware/q9_3_mobile/swdetail.php?pid=92"],
+				["Q9_ipad8.png",	"http://www.q9tech.com/web10/productIntro/retailSoftware/q9_3_mobile/swdetail.php?pid=93"],
+				["quickRulerbannerad470x210.png",	"https://play.google.com/store/apps/details?id=com.q9tech.tool.quickruler"],
+				["Q9_Android2.gif",	"https://play.google.com/store/apps/details?id=com.q9ime.inputmethod.q9latin"],
+				//["Q9_iphoneiOS7.gif", "http://www.q9tech.com/web10/productIntro/retailSoftware/q9_3_mobile/swdetail.php?pid=5"],
+				["q9qc_android_3users.png",		"http://q9tech.com/web10/news/latest/news.php?itemID=24"],
+				//["Q9ipad_copynpaste.gif", "http://itunes.apple.com/hk/app/id633331890"],
+				["msf_org.jpg",		"http://www.msf.org.hk"]			
+				];
+*/			
+var banners = [	
+				["https://lh3.googleusercontent.com/YR3cMA03_K67IiwcawI5iMG5myTqSQWV-fs7ZwDs7Wlb2yNPC8zkiIvIf8F33fjyFACsxgDVu6EvBDCNV12Y3cmQJO0hXDIinCYQ1Oba12lvONxIvRjlwpcjwWPVruLZxIc4_C7auQ=w470-h210-no",	
+				"http://www.q9tech.com/web10/productIntro/retailSoftware/qd_ebook_1_mobile/swdetail.php?pid=110"],				
+				
+				["https://lh3.googleusercontent.com/f7H02j2KFkReJsqRYOv90dEarsG0oCm9N0RQ1gfyPCQHNbEBLYuIXhoysQPvErm5zDrwT0vixt_PKKHth34It529lwiRPt0gB7lMK8QxppOUuVAEatTBgZhqEk8uVoAF5A_NT53xgg=w470-h210-no",	
+				"http://www.q9tech.com/web10/productIntro/retailSoftware/q9_3_mobile/swdetail.php?pid=92"],
+				
+				["https://lh3.googleusercontent.com/sHqAmzN0QI71ckw4uET9caYnMaHhIBKQox2OV_VZILd1eZPcQOPCBThL71QVUglY89F4CViCEdv_nlncOVX_QzjBr6jFeSEAriwOs9mwnyFCSn3MJMuAlCN0J4B1TKFI0m3xSbWDyg=w470-h210-no",	
+				"http://www.q9tech.com/web10/productIntro/retailSoftware/q9_3_mobile/swdetail.php?pid=93"],
+				
+				["https://lh3.googleusercontent.com/fT-GcS4tCkAWTQK5_k_0pQ_k0VhzHka2WlG5DlFUs6IorknuWm8V9CxmbMPAxjJnxBy9fbDPqDqNBQlXWHwv2J6_Q7PM-QySsTqf--QTGIEYIwbVXLxqU4JEQO3paD8FGP2w02G3yw=w470-h210-no",	
+				"https://play.google.com/store/apps/details?id=com.q9tech.tool.quickruler"],
+				
+				["https://lh3.googleusercontent.com/19HOvWqqmsneIyDvGYw_Erb0m2HgJsiPY8JpCs7pVfbQN2y2l2ozJ0RmHSMJpP4jbT0hALPPca2fb7s2CqVGZARpyCe3K2NaEUtEXUuaYzBNGkIcsPf9RCskwweG93MvY342gTFLvg=w470-h210-no",	
+				"https://play.google.com/store/apps/details?id=com.q9ime.inputmethod.q9latin"],
+				
+				["https://lh3.googleusercontent.com/0l-WTyZvani6hO63COTW3KT9EeuhsdbQE50tXnjEYfIJD4LKG58g1zyaLl1lHJOXKf-AeU-TCsQ8Knzx45j4QIehPzNpPo0ACKi8WSzkCA56Y5EhHw5C2MeCegLttJK9kvVf4N4bUQ=w470-h210-no",		
+				"http://q9tech.com/web10/news/latest/news.php?itemID=24"],
+				
+				["https://lh3.googleusercontent.com/DyAEywWnWg7i8RmN8b9maCWk-qqAP1SkS3LdzC8HLyO6Mzz7s-xOIT56ZDVn6N-hxENeumIX_WPEsgcroR0Z6A0i1o3Ws9Oz21paRiBcpSm2Ap_5eM2F31yQ07HEeM8gWifUttSSnQ=w470-h210-no",		
+				"http://www.msf.org.hk"]			
+				];
+
+//for (var i = 0; i < banners.length; i++) banners[i][0]="http://googledrive.com/host/0B_pTmStVvEpDekZYVXJUSEphTUE/"+banners[i][0];
+//http://googledrive.com/host/0B_pTmStVvEpDekZYVXJUSEphTUE/
+	
+var bannerPath = "data1/images/";
+var startwow=0;
+
+var now = new Date();
+var lastBanner="";
+for (var i = 0; i < banner_fastival.length; i++) 
+{
+	var fa = new Date();
+	fa.setDate(banner_fastival[i][1]);
+	fa.setMonth(banner_fastival[i][0]-1);
+	
+	var timeDiff = now.getTime()-fa.getTime();
+	var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+	//alert( (banner_fastival[i][2]) + " " +  diffDays + " " +  (banner_fastival[i][3]))	;
+	if (banner_fastival[i][2]<=diffDays && diffDays<=banner_fastival[i][3])  		
+	{
+		var imgPath=bannerPath+banner_fastival[i][4];
+		if (banner_fastival[i][4].indexOf("http")==0) imgPath=banner_fastival[i][4];
+		if (lastBanner==imgPath) continue;
+		document.write('<li><a href="'+banner_fastival[i][5]+'" target="_blank"><img src="'+imgPath+'" alt="" title="" id="wows1_'+ theI +'"/></a></li>');		
+		lastBanner=imgPath;
+		startwow++;
+	}
+}
+
+
+for (var i = 0; i < banners.length; i++) 
+{	
+	var theI=startwow+i;
+	var imgPath=bannerPath+banners[i][0];
+	if (banners[i][0].indexOf("http")==0) imgPath=banners[i][0];
+    document.write('<li><a href="'+banners[i][1]+'" target="_blank"><img src="'+imgPath+'" alt="" title="" id="wows1_'+ theI +'"/></a></li>');
+
+}
+
